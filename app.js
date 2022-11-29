f4bdd7de234a73dc07e82ba16b265c50ddcbad7e
Hector Padilla <hpadilla6201@gmail.com>
2022-11-29 12:49:47 -0500

    a9

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
+import mongoose from "mongoose";
+const DB_CONNECTION_STRING = 'mongodb+srv://hpadilla6201:Cristiano7!!@cluster0.26fhyow.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 
 const app = express()
 app.use(cors())
