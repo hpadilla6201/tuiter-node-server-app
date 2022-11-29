ce6bd1e5d11e950a123b1c89a51b0adf99c1456d
Hector Padilla <hpadilla6201@gmail.com>
2022-11-29 15:54:33 -0500

    a9

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
 import mongoose from "mongoose";
-const DB_CONNECTION_STRING = 'mongodb+srv://hpadilla6201:Cristiano7!!@cluster0.26fhyow.mongodb.net/?retryWrites=true&w=majority'
+const DB_CONNECTION_STRING = 'mongodb+srv://hpadilla6201:<Cristiano7!!>@cluster0.26fhyow.mongodb.net/?retryWrites=true&w=majority'
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
  || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,16 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  disliked: Number,
+  handle: String,
+  image: String, 
+  replies: Number,
+  retuits: Number,
+  time: String,
+  title: String,
+  topic: String,
+  username: String,
+  
 }, {collection: 'tuits'});
 export default schema;
+
